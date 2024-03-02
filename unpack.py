291ff86b2255a4de58f7a1d8ad81a1efe24e5fcf
dagrawa2 <dagrawa2@vols.utk.edu>
2024-03-02 11:22:43 -0500

    Remove config from unpack.

--- unpack.py
+++ unpack.py
@@ -1,18 +1,8 @@
 import os
 import gzip
-import configparser
-
-
-def load_config():
-	cfg = configparser.ConfigParser()
-	cfg.read('config.ini')
-	return cfg
-
 
 if __name__ == '__main__':
-	cfg = load_config()
-
-	metadata_dir = cfg['paths']['metadata_dir']
+	metadata_dir = 'data'
 
 	filenames = os.listdir(metadata_dir)
 	total = sum([1 for filename in filenames if filename.endswith('.xml.gz')])
